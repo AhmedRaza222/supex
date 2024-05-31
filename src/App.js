2c00ab21ee44dc70370be0752c81fd45b3b0ddf6
Ahmed Raza <ahmed.raza@devorbis.com>
2024-06-01 00:42:24 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Drawer from "./components/drawer/Drawer";
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Library from './components/libray/Library'
+import Annales from './components/annales/Annales'
+import Planner from './components/planner/Planner'
+import Playlists from './components/playlists/Playlists'
+import History from './components/history/History'
+import styles from "./App.module.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      <BrowserRouter>
+        <Drawer />
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/annales" element={<Annales />} />
+            <Route path="/playlists" element={<Playlists />} />
+            <Route path="/planner" element={<Planner />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/" element={<Home />} /> {/* Adding default route */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
